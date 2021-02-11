284d0d00967758c7594e9edf28c24fe3e769b8a6
abhi040 <74364545+abhi040@users.noreply.github.com>
2021-02-11 14:13:18 +0530

    Add files via upload

---
+++ number_guess.py
@@ -0,0 +1,19 @@
+num=15
+i=5
+while(i>0):
+    # print("choices left", i)
+    var=int(input("Enter the number of your choice: "))
+    if var==num:
+        print("Congratulations!You got the right guess")
+        break
+    elif var<num:
+        print("Enter the grater number")
+    elif var>num:
+        print("Enter the smaller number")
+    else:
+        print("You have no choice left")
+    i = i - 1
+    print("choices left", i)
+    continue
+
+
