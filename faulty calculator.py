a8a929f73406969d84f772d22afb86772c10d951
abhi040 <74364545+abhi040@users.noreply.github.com>
2021-02-11 13:41:53 +0530

    Add files via upload

---
+++ faulty calculator.py
@@ -0,0 +1,24 @@
+op = input("Enter the operator you want to use: ")
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+if op == '+':
+    if a==56 and b==9:
+        print(77)
+    else:
+        c = a+b
+        print(c)
+elif op == '-':
+    c = a-b
+    print(c)
+elif op == '*':
+    if a==45 and b==3:
+        print(555)
+    else:
+        c = a*b
+        print(c)
+elif op == '/':
+    if a==56 and b==6:
+        print(4)
+    else:
+        c = a/b
+        print(c)
